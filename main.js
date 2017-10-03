aa9b69f228959fe96d0e2eebb75c703365ff2221
Gim YoungPhil <youngphil.gim@gmail.com>
2017-10-04 03:11:49 +0900

    fixed main.js

--- main.js
+++ main.js
@@ -1,5 +1,52 @@
-d3.select("#d3")
-  .selectAll("p")
-  .data([4, 8, 15, 16, 23, 42])
-  .enter().append("p")
-  .text(function(d) { return "I’m number " + d + "!"; });
+let sakeData = [
+  {
+    "Product": "하쿠시카 준마이 팩",
+    "NihonShudo": 2,
+    "Acidity": 1.4
+  },
+  {
+    "Product": "하쿠시카 준마이 팩1",
+    "NihonShudo": 2,
+    "Acidity": 1.6
+  },
+  {
+    "Product": "하쿠시카 준마이 팩2",
+    "NihonShudo": 6,
+    "Acidity": 1.5
+  },
+  {
+    "Product": "하쿠시카 준마이 팩3",
+    "NihonShudo": 9,
+    "Acidity": 1.2
+  }
+];
+
+
+
+const xExtent = [0.4, 2.8];
+const yExtend = [20, -30];
+
+const xScale = d3.scaleLinear().domain(xExtent).range([0, 500]);
+const yScale = d3.scaleLinear().domain(yExtend).range([0, 500]);
+
+const xAxis = d3.axisBottom(xScale).tickSize(500);
+d3.select("svg")
+  .append("g")
+  .attr("id", "xAxisG")
+  // .attr("transform", "translate(0,499)")
+  .call(xAxis);
+
+const yAxis = d3.axisRight(yScale).tickSize(500);
+d3.select("svg")
+  .append("g")
+  .attr("id", "yAxisG")
+  .call(yAxis);
+
+d3.select("svg")
+  .selectAll("circle")
+  .data(sakeData)
+  .enter()
+  .append("circle")
+  .attr("r", 5)
+  .attr("cx", (d) => xScale(d.Acidity))
+  .attr("cy", (d) => yScale(d.NihonShudo));
