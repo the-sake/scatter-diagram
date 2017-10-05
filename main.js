51087dc65eceb582e4d0162bb4352fcabf5f497e
Gim YoungPhil <youngphil.gim@gmail.com>
2017-10-06 02:49:03 +0900

    fixed main

--- main.js
+++ main.js
@@ -1,34 +1,46 @@
 const typeData = [
   {
-    name: "x11",
+    name: "淡麗辛口",
+    mean: "담백한 카라쿠치",
     color: "#a7d317",
     x: 0,
-    y: 0
+    y: 0,
+    tX: 200,
+    tY: 40
   },
   {
-    name: "x12",
+    name: "濃醇辛口",
+    mean: "농후한 카라쿠치",
     color: "#e95696",
     x: 350,
-    y: 0
+    y: 0,
+    tX: 450,
+    tY: 200
   },
   {
-    name: "x13",
+    name: "淡麗甘口",
+    mean: "담백한 아마쿠치",
     color: "#d3e96b",
     x: 0,
-    y: 350
+    y: 350,
+    tX: 50,
+    tY: 300
   },
   {
-    name: "x14",
+    name: "濃醇甘口",
+    mean: "농후한 아마쿠치",
     color: "#ee93bc",
     x: 350,
-    y: 350
+    y: 350,
+    tX: 300,
+    tY: 480
   }
 ]
 
 d3.select('svg')
   .append('g')
-  .attr('id', 'backgroundG')
-  .attr('transform', 'rotate(30 450 150)')
+  .attr('id', 'bgGroup')
+  .attr('transform', 'rotate(30 450 70)')
   .selectAll('rect')
   .data(typeData)
   .enter()
@@ -38,7 +50,20 @@ d3.select('svg')
   .attr('y', d => d.y)
   .attr('width', 350)
   .attr('height', 350)
-  .style('fill', d => d.color);
+  .style('fill', d => d.color)
+
+d3.select("svg")
+  .append("g")
+  .attr("id", "txtGroup")
+  .selectAll("text")
+  .data(typeData)
+  .enter()
+  .append("text")
+  .style("text-anchor", "middle")
+  .attr("y", d => d.tY)
+  .attr("x", d => d.tX)
+  .style("font-size", "20px")
+  .text(d => d.name)
 
   const sakeData = [
     {
@@ -63,24 +88,35 @@ d3.select('svg')
     }
   ];
 
+// 일본주도
 const xExtent = [0.4, 2.8];
-const yExtend = [20, -30];
+// 산도
+const yExtend = [25, -30];
 
 const xScale = d3.scaleLinear().domain(xExtent).range([0, 500]);
 const yScale = d3.scaleLinear().domain(yExtend).range([0, 500]);
 
-const xAxis = d3.axisBottom(xScale).tickSize(500);
+const xAxis = d3.axisBottom(xScale)
+                .tickSize(500);
 d3.select("svg")
   .append("g")
-  .attr("id", "xAxisGf")
-  // .attr("transform", "translate(0,499)")
-  .call(xAxis);
+  .attr("id", "xAxisG")
+  .call(g => {
+    g.call(xAxis);
+    g.select(".domain").remove();
+    g.selectAll(".tick:not(:last-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "3,3");
+  });
 
-const yAxis = d3.axisRight(yScale).tickSize(500);
+const yAxis = d3.axisRight(yScale)
+                .tickSize(500);
 d3.select("svg")
   .append("g")
   .attr("id", "yAxisG")
-  .call(yAxis);
+  .call(g => {
+    g.call(yAxis);
+    g.select(".domain").remove();
+    g.selectAll(".tick:not(:last-of-type) line").attr("stroke", "#777").attr("stroke-dasharray", "3,3");
+  });
 
 d3.select("svg")
   .selectAll("circle")
